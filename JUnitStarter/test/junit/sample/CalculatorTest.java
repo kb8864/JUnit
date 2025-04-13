63ac5f487f8d6821f14b59d92e0abbc22b62fa27
user <user@DESKTOP-BJKKHFA>
2025-04-13 17:19:27 +0900

    例外テストの実施

--- JUnitStarter/test/junit/sample/CalculatorTest.java
+++ JUnitStarter/test/junit/sample/CalculatorTest.java
@@ -11,9 +11,9 @@ public class CalculatorTest {
 		Calculator cal = new Calculator();
 		//期待値
 		int expected = 5;
-//		実測値
+		//		実測値
 		int actual = cal.add(3, 2);
-//		期待値と実測値の比較
+		//		期待値と実測値の比較
 		assertEquals(expected, actual);
 	}
 
@@ -40,6 +40,25 @@ public class CalculatorTest {
 		float actual = cal.div(8, 2);
 		assertEquals(expected, actual);
 
+	}
+
+	@Test
+	public void testDivException() {
+		Calculator cal = new Calculator();
+//		第一引数は例外クラスを、第二引数はラムダ式でメソッドの実行記入
+		assertThrows(IllegalArgumentException.class, () -> cal.div(3, 0));
+	}
+
+	@Test
+	public void testDivException2() {
+		Calculator cal = new Calculator();
+		try {
+			cal.div(3, 0);
+				fail();
+		}catch(IllegalArgumentException e) {
+			assertEquals("第二引数に0が指定されました", e.getMessage());
+
+		}
 	}
 
 }
