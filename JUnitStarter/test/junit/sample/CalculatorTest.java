fd95346f0ac997f48167e5d3280d131d7672866d
user <user@DESKTOP-BJKKHFA>
2025-04-13 16:41:21 +0900

    Revert "Revert "初めてのテストコード作成""
    This reverts commit e966e410baf58fc0b9f3f255e36510e7aa87ed64.

--- JUnitStarter/test/junit/sample/CalculatorTest.java
+++ JUnitStarter/test/junit/sample/CalculatorTest.java
@@ -1,27 +1,45 @@
 package junit.sample;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
+public class CalculatorTest {
 
 	@Test
-	void testAdd() {
-//		fail("まだ実装されていません");
+	public void testAdd() {
+		Calculator cal = new Calculator();
+		//期待値
+		int expected = 5;
+//		実測値
+		int actual = cal.add(3, 2);
+//		期待値と実測値の比較
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testSub() {
-//		fail("まだ実装されていません");
+	public void testSub() {
+		Calculator cal = new Calculator();
+		int expected = 5;
+		int actual = cal.sub(7, 2);
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testMul() {
-//		fail("まだ実装されていません");
+	public void testMul() {
+		Calculator cal = new Calculator();
+		int expected = 14;
+		int actual = cal.mul(7, 2);
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testDiv() {
-//		fail("まだ実装されていません");
+	public void testDiv() {
+		Calculator cal = new Calculator();
+		int expected = 4;
+		float actual = cal.div(8, 2);
+		assertEquals(expected, actual);
+
 	}
 
 }
