2fa424e72c8fb253fff56e940d87fe8de87cf75e
user <user@DESKTOP-BJKKHFA>
2025-04-14 01:20:16 +0900

    @DisabledとassertTrueとassumingThatの使い方

--- JUnitStarter/test/junit/sample/CalculatorTest.java
+++ JUnitStarter/test/junit/sample/CalculatorTest.java
@@ -1,11 +1,12 @@
 package junit.sample;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 public class CalculatorTest {
 	Calculator cal;
 
@@ -17,6 +18,7 @@ public class CalculatorTest {
 
 	@Test
 	public void testAdd() {
+		assertTrue("DEV".equals(System.getenv("ENV")));
 		//期待値
 		int expected = 5;
 		//		実測値
@@ -27,9 +29,14 @@ public class CalculatorTest {
 
 	@Test
 	public void testSub() {
-		int expected = 5;
-		int actual = cal.sub(7, 2);
-		assertEquals(expected, actual);
+		assumingThat("DEV".equals(System.getenv("ENV")), () -> {
+			int expected = 5;
+			int actual = cal.sub(7, 2);
+			assertEquals(expected, actual);
+			System.out.println("(１)テスト実行");
+		});
+		System.out.println("(２)テスト終了");
+
 	}
 
 	@Test
@@ -50,17 +57,18 @@ public class CalculatorTest {
 	@Test
 	@Tag("Exception")
 	public void testDivException() {
-//		第一引数は例外クラスを、第二引数はラムダ式でメソッドの実行記入
+		//		第一引数は例外クラスを、第二引数はラムダ式でメソッドの実行記入
 		assertThrows(IllegalArgumentException.class, () -> cal.div(3, 0));
 	}
 
+	@Disabled
 	@Test
 	@Tag("Exception")
 	public void testDivException2() {
 		try {
 			cal.div(3, 0);
-				fail();
-		}catch(IllegalArgumentException e) {
+			fail();
+		} catch (IllegalArgumentException e) {
 			assertEquals("第二引数に0が指定されました", e.getMessage());
 
 		}
