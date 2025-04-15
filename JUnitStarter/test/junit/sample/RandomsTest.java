e6f9e2a6cd1987164887e308c2557463a69675e7
user <user@DESKTOP-BJKKHFA>
2025-04-15 13:40:31 +0900

    スタブの学習

--- JUnitStarter/test/junit/sample/RandomsTest.java
+++ JUnitStarter/test/junit/sample/RandomsTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +16,24 @@ public class RandomsTest{
 		options.add("A");
 		options.add("B");
 		Randoms sut = new Randoms();
-		//スタブの設定
 
+//		モックの生成
+		final AtomicBoolean isCalled = new AtomicBoolean(false);
+		//テストメソッドにスタブの設定
+		sut.random = new RandomNumber() {
+
+			@Override
+			public int nextInt() {
+				// TODO 自動生成されたメソッド・スタブ
+				isCalled.set(true);
+				return 0;
+			}
+		};
+//		//別途でスタブクラスの生成
+//		sut.random = new RandomNumberStub();
 		assertEquals(sut.choice(options), "A");
+		
+		assertTrue(isCalled.get());
 	}
 
 }
